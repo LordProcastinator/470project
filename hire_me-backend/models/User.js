3696cd3f57e808971bf659fc083628c6a5d4a3e1
LordProcastinator <gfdao69@gmail.com>
2025-04-30 22:06:44 +0600

    mvc backend added

---
+++ hire_me-backend/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone_number: { type: String },
+  profile_picture: { type: String },
+  address: { type: String },
+  role: { type: String, required: true, default: 'user' }
+}, { timestamps: true });
+
+// Exclude password from the response
+userSchema.methods.toJSON = function() {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
